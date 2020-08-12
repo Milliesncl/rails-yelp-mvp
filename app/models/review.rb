cb3fa4ff45d6aa19906699732404cbb3f635307b
Millie Senecal <millie.senecal@gmail.com>
2020-08-12 13:32:50 -0400

    all model and validation done

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
 
   # validates :rating, inclusion: { in: %w('1..5'), message: "%{value} is not a valid rating, should be between 1 & 5" }
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-  validates :content, length: { minimum: 10 }
+  validates :content, presence: true
 end
